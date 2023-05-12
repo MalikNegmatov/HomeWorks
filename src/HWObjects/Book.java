634320a096802881b38e1cc9fc65951e9a2129c9
MalikNegmatov <maliknegmatov@gmail.com>
2023-05-12 15:48:19 +0300

    Домашнее задание объекты и классы

--- src/HWObjects/Book.java
+++ src/HWObjects/Book.java
@@ -1,4 +1,65 @@
 package HWObjects;
 
 public class Book {
+    String title;   // Название книги
+    Author author = new Author(); // Автор книги
+    int year;  // Год издания книги
+
+    // Конструктор по умолчанию.
+    Book() {
+    }
+    /* Безусловная инициализация полей по умолчанию. При создании объект  всегда создает книгу
+     по умолчанию: "Наша Марка" // Иван Николаевич Бездомный, 1935 г. */
+    {
+        title = "Наша Марка";
+        year = 1935;
+    }
+
+    /**
+     * Конструктор класса
+     * @param title - название книги
+     * @param author - автор книги
+     * @param year - год издания
+     */
+    Book(String title, Author author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+    public String getAuthorName() {
+        return this.author.fullName();
+    }
+
+    public int getYear(){
+        return this.year;
+    }
+
+    public void setTile(String title){
+        this.title = title;
+    }
+
+    public void setAuthor(Author author){
+        this.author = author;
+    }
+
+    public void setYear (int year){
+        this.year = year;
+    }
+
+    /**
+     * Метод склеивания в строку полного титула книги для цитирования
+     * @return строка составленная из значений полей объекта библиографической ссылки.
+     */
+    public String citeBook() {
+        return "\"" + this.title + ": " + this.author.fullName() + ": " + this.year + "\";";
+    }
+
 }
--- src/HWObjects/Main.java
+++ src/HWObjects/Main.java
@@ -2,11 +2,99 @@ package HWObjects;
 
 public class Main {
 
+    public static Book testBook = new Book(), warAndPeace, theatricalNovel;
     public static void main(String[] args) {
-        // Задачи блока Методы
+        // Задачи блока Объекты и классы
         task1();
+        // Дополнительная задача блока Объекты и классы
+        task2();
+    }
+
+    public static void printAuthor(String author) {
+        System.out.println(author);
+    }
+
+    public static void printAuthor(Author author) {
+        String fullName = author.getFirstName() + " " + author.getMiddleName() + " " + author.getLastName();
+        System.out.println(fullName);
+    }
+    public static void testAuthor() {
+        Author testAuthor = new Author();
+        printAuthor(testAuthor);
+        testAuthor.setFirstName("George");
+        testAuthor.setMiddleName("Bernard");
+        testAuthor.setLastName("Shaw");
+        printAuthor(testAuthor.fullName());
+
+        Author levTolstoy = new Author("Лев", "Николаевич", "Толстой");
+        Author mikhailBulgakov = new Author("Михаил Афанасьевич Булгаков");
+        printAuthor(levTolstoy.fullName());
+        printAuthor(mikhailBulgakov);
+    }
+
+    public static void printBook(String book){
+        System.out.println(book);
+    }
+
+    public static void printBook(Book book){
+        String cite = "\"" + book.getTitle() + "\" // " + book.getAuthorName() + ", " + book.getYear() + " г.";
+        System.out.println(cite);
+    }
+    public static void testBook() {
+        printBook(testBook);
+        testBook.setTile("Too True To Be Good");
+        Author shaw = new Author("George Bernard Shaw");
+        testBook.setAuthor(shaw);
+        testBook.setYear(1931);
+        printBook(testBook.citeBook());
+
+        Author levTolstoy = new Author("Лев", "Николаевич", "Толстой");
+        Author mikhailBulgakov = new Author("Михаил Афанасьевич Булгаков");
+
+        warAndPeace = new Book("Война и мир", levTolstoy, 1867);
+        printBook(warAndPeace.citeBook());
+        Author whoIs = warAndPeace.getAuthor();
+        printAuthor(whoIs);
+
+        theatricalNovel = new Book("Театральный роман", mikhailBulgakov, 1936);
+        printBook(theatricalNovel.citeBook());
+        theatricalNovel.setYear(1967);
+        printBook(theatricalNovel.citeBook());
     }
 
     public static void task1() {
+        System.out.println("Задача №1");
+        testAuthor();
+        System.out.println();
+        testBook();
+        System.out.println();
+    }
+    public static void task2() {
+        System.out.println("Задача №2");
+        Library myLibrary = new Library(5);
+
+        myLibrary.addBook(warAndPeace);
+        myLibrary.addBook(testBook);
+        myLibrary.addBook(theatricalNovel);
+        Author author = new Author("Лев Николаевич Толстой");
+        Book book = new Book("Анна Каренина", author, 1875);
+        myLibrary.addBook(book);
+        book = new Book("Воскресение", author, 1899);
+        myLibrary.addBook(book);
+        book = new Book("Декабристы", author, 1884);
+        myLibrary.addBook(book);
+
+        myLibrary.replaceBook(book,3);
+        System.out.println();
+
+        myLibrary.printLibrary();
+        System.out.println();
+
+        myLibrary.replaceYear(theatricalNovel.getTitle(),1936);
+        System.out.println();
+        myLibrary.printLibrary();
+        System.out.println();
+
+        myLibrary.replaceYear("Воскрес",1936);
     }
 }
