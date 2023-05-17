5b1132fcc490a4738e5a38eaf93331133bca4371
MalikNegmatov <maliknegmatov@gmail.com>
2023-05-17 12:42:17 +0300

    Курсовая черновик. Методы форматирования 3.

--- src/Сourse_base/Employee.java
+++ src/Сourse_base/Employee.java
@@ -7,7 +7,7 @@ public class Employee {
     private int department;     // отдел
     private int salary;         // зарплата
     public static int id = 1;   // счетчик для присваивания объекту класса уникального id
-    private int idEmployee;     // id сотрудника
+    private final int idEmployee;     // id сотрудника
 
     /** методы get и set для всех полей класса */
     public String getFirstName() {
@@ -44,13 +44,73 @@ public class Employee {
         this.salary = salary;
     }
     public Employee(String firstName, String middleName, String lastName, int department, int salary) {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
+        this.firstName = formatStr(firstName);
+        this.middleName = formatStr(middleName);
+        this.lastName = formatStr(lastName);
         this.department = department;
         this.salary = salary;
         this.idEmployee = id;
         id++;
     }
 
+    public Employee() {
+        java.util.Random random = new java.util.Random();
+        this.firstName = fNames[random.nextInt(21)];
+        this.middleName = mNames[random.nextInt(21)];
+        this.lastName = lNames[random.nextInt(21)];
+        this.department = random.nextInt(5);
+        this.salary = random.nextInt(200000) + 40000;
+        this.idEmployee = id;
+        id++;
+    }
+    public String formatStr(String str) {
+        str = checkInputErrors(str);
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+    public String checkInputErrors(String str) {
+        if (str != null && str.length() > 0) {
+            str = str.replaceAll("[^\\а-яa-zA-zА-ЯЁё]", "");
+        }
+        return str;
+    }
+    @Override
+    public String toString() {
+        return "ID: " + this.idEmployee + "; ФИО: " + this.firstName + " " + this.middleName + " " + this.lastName
+                + "; Отдел №" + this.department + "; ЗП:" + this.salary + ".";
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+
+        Employee employee = (Employee) other;
+
+        if (department != employee.department) return false;
+        if (salary != employee.salary) return false;
+        if (idEmployee != employee.idEmployee) return false;
+        if (!firstName.equals(employee.firstName)) return false;
+        if (!middleName.equals(employee.middleName)) return false;
+        return lastName.equals(employee.lastName);
+    }
+    @Override
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + middleName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + department;
+        result = 31 * result + salary;
+        result = 31 * result + idEmployee;
+        return result;
+    }
+
+    private String[] fNames = new String[] {"Иван", "Сергей", "Борис", "Павел", "Геннадий", "Алексей", "Александр",
+    "Виталий", "Олег", "Василий", "Петр", "Руслан", "Марат", "Владимир", "Дмитрий", "Радион", "Федор",
+    "Семен", "Кирилл", "Андрей", "Феликс"};
+    private String[] mNames = new String[] {"Иванович", "Сергеевич", "Борисович", "Павлович", "Геннадьевич",
+            "Алексеевич", "Александрович", "Витальевич", "Олегович", "Васильевич", "Петрович", "Русланович",
+            "Маратович", "Владимирович", "Дмитриевич", "Радионович", "Федорович", "Семенович", "Кириллович",
+            "Андреевич", "Константинович"};
+    private String[] lNames = new String[] {"Хорошилов", "Сергеев", "Борисов", "Павлов", "Семашко",
+    "Васильев", "Родионов", "Прохоров", "Райкунов", "Малышев", "Точилин", "Сгибнев", "Фоминов",
+    "Макаров", "Захаренко", "Шевченко", "Маслов", "Курбатов", "Шеин", "Рязанов", "Егоров"};
 }
--- src/Сourse_base/Main.java
+++ src/Сourse_base/Main.java
@@ -1,15 +1,23 @@
 package Сourse_base;
 
 public class Main {
-    public static Employee employeeList[] = new Employee[10];
+    public static Employee employeeList[] = new Employee[20];
     public static void main(String[] args) {
-        employeeList[0] = new Employee("Иван", null, "Комисcаржевский", 1, 30000);
-        System.out.println(employeeList[0].getFirstName());
+
+        employeeList[0] = new Employee("ИВ АН", "вАСИЛ ьЁвич", "Комис434cаржевский", 1, 30000);
+
+        for (int i = 1; i < 20; i++){
+            employeeList[i] = new Employee();
+        }
+
+        System.out.println(employeeList[0].formatStr(employeeList[0].getFirstName()));
         System.out.println(employeeList[0].getMiddleName());
         System.out.println(employeeList[0].getLastName());
         System.out.println(employeeList[0].getIdEmployee());
 
-        String test = "godunov32 av";
-        System.out.println(ch);
+        for (int i = 0; i < 20; i++){
+            System.out.println(employeeList[i]);
+        }
+
     }
 }
